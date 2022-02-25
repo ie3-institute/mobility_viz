9d23bc411b9ee29f8f29934e815d67272bae8b24
Kittl <chris.kittl@tu-dortmund.de>
2022-02-25 17:46:40 +0100

    Fetch data from database

--- emob_viz/Viz.py
+++ emob_viz/Viz.py
@@ -5,5 +5,14 @@ from emob_viz.Source import Source
 source = Source.with_defaults("postgres", "postgres", "postgres1234!")
 try:
     connection = source.get_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM novagent.poi LIMIT 5;")
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    # Don't forget to close the connection!
+    connection.close()
 except Exception as e:
     logging.error("Unable to connect to database.", e)
